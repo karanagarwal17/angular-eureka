3a8d24fc9206573cf3cd60b66e2fdfa3da15fad8
karanagarwal17 <karanagarwal1702@gmail.com>
2017-03-08 14:54:17 +0530

    started to work with angular ui router

---
+++ 11. Angular Ui Router/app/scripts/app.js
@@ -0,0 +1,64 @@
+'use strict';
+
+angular.module('confusionApp', ['ui.router'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+
+			.state('app', {
+			url: '/',
+			views: {
+				'header': {
+					templateUrl: 'views/header.html',
+				},
+				'content': {
+					template: '<h1>To be Completed</h1>',
+					controller: 'IndexController'
+				},
+				'footer': {
+					templateUrl: 'views/footer.html'
+				}
+			}
+		})
+
+		.state('app.aboutus', {
+			url: 'aboutus',
+			views: {
+				'content@': {
+					template: '<h1>To Be Completed</h1>',
+					controller: 'AboutController'
+				}
+			}
+		})
+
+		.state('app.contactus', {
+			url: 'contactus',
+			views: {
+				'content@': {
+					templateUrl: 'views/contactus.html',
+					controller: 'ContactController'
+				}
+			}
+		})
+
+		.state('app.menu', {
+			url: 'menu',
+			views: {
+				'content@': {
+					templateUrl: 'views/menu.html'
+					controller: 'MenuController'
+				}
+			}
+		})
+
+		.state('app.menu', {
+			url: 'menu/:id',
+			views: {
+				'contect@': {
+					templateUrl: 'views/menu.html',
+					controller: 'DishDetailController'
+				}
+			}
+		});
+
+		$urlRouterProvider.otherwise('/');
+	});
